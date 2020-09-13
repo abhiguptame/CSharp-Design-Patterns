ee4b9df6bada91e4b86cfc3b99b9e8e390bb760c
abhiguptame <abhiguptame@gmail.com>
2020-09-13 12:17:34 +0530

    Singleton Design Pattern

---
+++ Singleton/Singleton/Policy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Policy
+    {
+        //private static readonly object _lock = new object();
+
+        private static readonly Policy _instance = new Policy();
+
+        // Made thread Safe and removed the lock
+        public static Policy Instance {
+            get {
+                    return _instance;
+                }            
+        }
+
+        public Policy()
+        {
+
+        }
+        private int Id { get; set; } = 123;
+        private string Insured { get; set; } = "John Roy";
+
+        public string GetInsuredName() => Insured;
+    }
+}
